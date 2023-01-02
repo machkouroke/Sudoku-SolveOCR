afeb666d3000a6df62c87168c7d064252872c8b9
machkouroke <machkour20.mo1@gmail.com>
2023-01-02 18:56:44 +0000

    `build` Add of train digit classifier

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+import cv2 as cv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pyimagesearch.Sudoku.puzzle import find_puzzle
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+find_puzzle(cv.imread('puzzle.jpg'), debug=True)
